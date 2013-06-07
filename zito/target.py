6bc21443159117d31a54d79faa966c97e196ebce
Andrews Medina <andrewsmedina@gmail.com>
2013-06-07 10:22:20 -0300

    added an initial generator for add and sub.

--- zito/target.py
+++ zito/target.py
@@ -1,7 +1,22 @@
 import sys
 
 
+def generator(operation):
+    if operation == 'add':
+        def f(a, b):
+            return a + b
+    else:
+        def f(a, b):
+            return a - b
+    return f
+
+
+add = generator('add')
+sub = generator('sub')
+
+
 def entry_point(argv):
+    print add(sub(int(argv[1]), 3), 4)
     return 0
 
 
