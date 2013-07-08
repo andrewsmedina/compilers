30b703823cd12838fc554e109f36aab78406eea8
Andrews Medina <andrewsmedina@gmail.com>
2013-07-08 01:25:04 -0300

    added Token.

--- kaleidoscope/toy.cpp
+++ kaleidoscope/toy.cpp
@@ -1,3 +1,18 @@
+#include <cstdlib>
+#include <string>
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
+// of these for known things.
+enum Token {
+	tok_eof = -1,
+	// commands
+	tok_def = -2, tok_extern = -3,
+	// primary
+	tok_identifier = -4, tok_number = -5,
+};
+
+static std::string IdentifierStr;  // Filled in if tok_identifier
+static double NumVal;              // Filled in if tok_number
+
 int main() {
 	return 0;
 }
