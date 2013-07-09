714a9c44d0d34fdf7b220b7a31c00f550297aa93
Andrews Medina <andrewsmedina@gmail.com>
2013-07-09 01:11:14 -0300

    kaleidoscope: added gettoke function.

--- kaleidoscope/toy.cpp
+++ kaleidoscope/toy.cpp
@@ -13,6 +13,48 @@ enum Token {
 static std::string IdentifierStr;  // Filled in if tok_identifier
 static double NumVal;              // Filled in if tok_number
 
+static int gettok() {
+	static int LastChar = ' ';
+	while (isspace(LastChar))
+		LastChar = getchar();
+
+	if (isalpha(LastChar)) {
+		IdentifierStr = LastChar;
+		while (isalnum((LastChar = getchar())))
+			IdentifierStr += LastChar;
+
+		if (IdentifierStr == "def") return tok_def;
+		if (IdentifierStr == "extern") return tok_extern;
+		return tok_identifier;
+	}
+
+	if (isdigit(LastChar) || LastChar == '.') {
+		std::string NumStr;
+		do {
+			NumStr += LastChar;
+			LastChar = getchar();
+		} while (isdigit(LastChar) || LastChar == '.');
+
+		NumVal = strtod(NumStr.c_str(), 0);
+		return tok_number;
+	}
+
+	if (LastChar == '#') {
+		do LastChar = getchar();
+		while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+		if (LastChar != EOF)
+			return gettok();;
+	}
+
+	if (LastChar == EOF)
+		return tok_eof;
+
+	int ThisChar = LastChar;
+	LastChar = getchar();
+	return ThisChar;
+}
+
 int main() {
 	return 0;
 }
