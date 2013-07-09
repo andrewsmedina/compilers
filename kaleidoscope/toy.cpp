1ebddb1265335ad2baca570e967d45c88172df40
Andrews Medina <andrewsmedina@gmail.com>
2013-07-09 01:35:27 -0300

    kaleidoscope: added ast class for funciton prototype.

--- kaleidoscope/toy.cpp
+++ kaleidoscope/toy.cpp
@@ -105,6 +105,17 @@ public:
 		: Callee(callee), Args(args) {}
 };
 
+// PrototypeAST - This class represents the "prototype" for a function,
+// which captures its name, and its argument names (thus implicitly the number
+// of arguments the function takes).
+class PrototypeAST {
+	std::string Name;
+	std::vector<std::string> Args;
+public:
+	PrototypeAST(const std::string &name, const std::vector<std::string> &args)
+		: Name(name), Args(args) {}
+};
+
 int main() {
 	return 0;
 }
