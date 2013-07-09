f3d439d14607075d803834c46d220ab1c0c33b08
Andrews Medina <andrewsmedina@gmail.com>
2013-07-09 01:16:12 -0300

    kaleidoscope: adde some braces.

--- kaleidoscope/toy.cpp
+++ kaleidoscope/toy.cpp
@@ -1,13 +1,14 @@
 #include <cstdlib>
 #include <string>
-// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
-// of these for known things.
+
 enum Token {
 	tok_eof = -1,
 	// commands
-	tok_def = -2, tok_extern = -3,
+	tok_def = -2,
+	tok_extern = -3,
 	// primary
-	tok_identifier = -4, tok_number = -5,
+	tok_identifier = -4,
+	tok_number = -5,
 };
 
 static std::string IdentifierStr;  // Filled in if tok_identifier
@@ -15,16 +16,23 @@ static double NumVal;              // Filled in if tok_number
 
 static int gettok() {
 	static int LastChar = ' ';
-	while (isspace(LastChar))
+
+	while (isspace(LastChar)) {
 		LastChar = getchar();
+	}
 
 	if (isalpha(LastChar)) {
 		IdentifierStr = LastChar;
-		while (isalnum((LastChar = getchar())))
+		while (isalnum((LastChar = getchar()))) {
 			IdentifierStr += LastChar;
+		}
 
-		if (IdentifierStr == "def") return tok_def;
-		if (IdentifierStr == "extern") return tok_extern;
+		if (IdentifierStr == "def") {
+			return tok_def;
+		}
+		if (IdentifierStr == "extern") {
+			return tok_extern;
+		}
 		return tok_identifier;
 	}
 
@@ -40,15 +48,18 @@ static int gettok() {
 	}
 
 	if (LastChar == '#') {
-		do LastChar = getchar();
-		while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+		do {
+			LastChar = getchar();
+		} while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
 
-		if (LastChar != EOF)
-			return gettok();;
+		if (LastChar != EOF) {
+			return gettok();
+		}
 	}
 
-	if (LastChar == EOF)
+	if (LastChar == EOF) {
 		return tok_eof;
+	}
 
 	int ThisChar = LastChar;
 	LastChar = getchar();
