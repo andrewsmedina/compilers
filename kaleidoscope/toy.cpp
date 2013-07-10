1b337e5ca4c549fc392e79bb4e05fed210bda5fa
Andrews Medina <andrewsmedina@gmail.com>
2013-07-09 23:50:33 -0300

    kaleidoscope: parsing the rest.

--- kaleidoscope/toy.cpp
+++ kaleidoscope/toy.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <map>
 
 enum Token {
 	tok_eof = -1,
@@ -154,6 +156,8 @@ ExprAST *Error(const char *Str) { fprintf(stderr, "Error: %s\n", Str);return 0;}
 PrototypeAST *ErrorP(const char *Str) { Error(Str); return 0; }
 FunctionAST *ErrorF(const char *Str) { Error(Str); return 0; }
 
+static ExprAST *ParseExpression();
+
 // numberexpr ::= number
 static ExprAST *ParseNumberExpr() {
 	ExprAST *Result = new NumberExprAST(NumVal);
@@ -226,6 +230,177 @@ static ExprAST *ParsePrimary() {
 	}
 }
 
+// binoprhs
+// ::= ('+' primary)*
+static ExprAST *ParseBinOpRHS(int ExprPrec, ExprAST *LHS) {
+	// If this is a binop, find its precedence.
+	while (1) {
+		int TokPrec = GetTokPrecedence();
+
+		// If this is a binop that binds at least as tightly as the current binop,
+		// consume it, otherwise we are done.
+		if (TokPrec < ExprPrec) {
+			return LHS;
+		}
+
+		// Okay, we know this is a binop.
+		int BinOp = CurTok;
+		getNextToken();  // eat binop
+
+		// Parse the primary expression after the binary operator.
+		ExprAST *RHS = ParsePrimary();
+		if (!RHS) {
+			return 0;
+		}
+
+		// If BinOp binds less tightly with RHS than the operator after RHS, let
+		// the pending operator take RHS as its LHS.
+		int NextPrec = GetTokPrecedence();
+		if (TokPrec < NextPrec) {
+			RHS = ParseBinOpRHS(TokPrec+1, RHS);
+			if (RHS == 0) {
+				return 0;
+			}
+		}
+
+		// Merge LHS/RHS.
+		LHS = new BinaryExprAST(BinOp, LHS, RHS);
+	}
+}
+
+// expression
+// ::= primary binoprhs
+static ExprAST *ParseExpression() {
+	ExprAST *LHS = ParsePrimary();
+	if (!LHS) {
+		return 0;
+	}
+	return ParseBinOpRHS(0, LHS);
+}
+
+// prototype
+// ::= id '(' id* ')'
+static PrototypeAST *ParsePrototype() {
+	if (CurTok != tok_identifier) {
+		return ErrorP("Expected function name in prototype");
+	}
+
+	std::string FnName = IdentifierStr;
+	getNextToken();
+
+	if (CurTok != '(') {
+		return ErrorP("Expected '(' in prototype");
+	}
+
+	std::vector<std::string> ArgNames;
+	while (getNextToken() == tok_identifier) {
+		ArgNames.push_back(IdentifierStr);
+	}
+
+	if (CurTok != ')') {
+		return ErrorP("Expected ')' in prototype");
+	}
+
+	// success.
+	getNextToken();  // eat ')'.
+
+	return new PrototypeAST(FnName, ArgNames);
+}
+
+// definition ::= 'def' prototype expression
+static FunctionAST *ParseDefinition() {
+	getNextToken();  // eat def.
+	PrototypeAST *Proto = ParsePrototype();
+	if (Proto == 0) {
+		return 0;
+	}
+
+	if (ExprAST *E = ParseExpression()) {
+		return new FunctionAST(Proto, E);
+	}
+	return 0;
+}
+
+// toplevelexpr ::= expression
+static FunctionAST *ParseTopLevelExpr() {
+	if (ExprAST *E = ParseExpression()) {
+		// Make an anonymous proto.
+		PrototypeAST *Proto = new PrototypeAST("", std::vector<std::string>());
+		return new FunctionAST(Proto, E);
+	}
+	return 0;
+}
+
+// external ::= 'extern' prototype
+static PrototypeAST *ParseExtern() {
+	getNextToken();  // eat extern.
+	return ParsePrototype();
+}
+
+//===----------------------------------------------------------------------===//
+// Top-Level parsing
+//===----------------------------------------------------------------------===//
+
+static void HandleDefinition() {
+	if (ParseDefinition()) {
+		fprintf(stderr, "Parsed a function definition.\n");
+	} else {
+		// Skip token for error recovery.
+		getNextToken();
+	}
+}
+
+static void HandleExtern() {
+	if (ParseExtern()) {
+		fprintf(stderr, "Parsed an extern\n");
+	} else {
+		// Skip token for error recovery.
+		getNextToken();
+	}
+}
+
+static void HandleTopLevelExpression() {
+	// Evaluate a top-level expression into an anonymous function.
+	if (ParseTopLevelExpr()) {
+		fprintf(stderr, "Parsed a top-level expr\n");
+	} else {
+		// Skip token for error recovery.
+	  	getNextToken();
+	}
+}
+
+// top ::= definition | external | expression | ';'
+static void MainLoop() {
+	while (1) {
+		fprintf(stderr, "ready> ");
+		switch (CurTok) {
+		case tok_eof:    return;
+		case ';':        getNextToken(); break;  // ignore top-level semicolons.
+		case tok_def:    HandleDefinition(); break;
+		case tok_extern: HandleExtern(); break;
+		default:         HandleTopLevelExpression(); break;
+		}
+	}
+}
+
+//===----------------------------------------------------------------------===//
+// Main driver code.
+//===----------------------------------------------------------------------===//
+
 int main() {
+	// Install standard binary operators.
+	// 1 is lowest precedence.
+	BinopPrecedence['<'] = 10;
+	BinopPrecedence['+'] = 20;
+	BinopPrecedence['-'] = 20;
+	BinopPrecedence['*'] = 40;  // highest.
+
+	// Prime the first token.
+	fprintf(stderr, "ready> ");
+	getNextToken();
+
+	// Run the main "interpreter loop" now.
+	MainLoop();
+
 	return 0;
 }
