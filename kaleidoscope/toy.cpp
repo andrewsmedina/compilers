6c7c9dceacaa0b4f9c75e80af10480016bd2f620
Andrews Medina <andrewsmedina@gmail.com>
2013-07-09 23:35:37 -0300

    kaleidoscope: implemented get token precedence.

--- kaleidoscope/toy.cpp
+++ kaleidoscope/toy.cpp
@@ -133,6 +133,22 @@ static int getNextToken() {
 	return CurTok = gettok();
 }
 
+// BinopPrecedence - This holds the precedence for each binary operator that is
+// defined.
+static std::map<char, int> BinopPrecedence;
+
+// GetTokPrecedence - Get the precedence of the pending binary operator token.
+static int GetTokPrecedence() {
+	if (!isascii(CurTok)) {
+		return -1;
+	}
+	// Make sure it's a declared binop.
+	int TokPrec = BinopPrecedence[CurTok];
+	if (TokPrec <= 0) return -1; {
+		return TokPrec;
+	}
+}
+
 // Error* - These are little helper functions for error handling.
 ExprAST *Error(const char *Str) { fprintf(stderr, "Error: %s\n", Str);return 0;}
 PrototypeAST *ErrorP(const char *Str) { Error(Str); return 0; }
