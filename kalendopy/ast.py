27c42e54dec0d2b20992327791f5f5182594c95a
Andrews Medina <andrewsmedina@gmail.com>
2013-07-12 01:48:20 -0300

    kalendopy: initial ast objects.

---
+++ kalendopy/ast.py
@@ -0,0 +1,24 @@
+class Node(object):
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return NotImplemented
+        return (type(self) is type(other) and
+                self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+
+class Block(Node):
+    def __init__(self, statements):
+        self.statements = statements
+
+
+class Statement(Node):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class Node(Node):
+    def __init__(self, value):
+        self.value = value
